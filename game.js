b452a4540144919731331df5f1f7a88d344d28b2
Kevin <kevinszeto9449@gmail.com>
2022-03-04 20:03:19 -0800

    Finished functitonality for a full game with 5 rounds

--- game.js
+++ game.js
@@ -1,5 +1,3 @@
-console.log("Hello world");
-
 function computerPlay() {
     let choice = Math.floor(Math.random() * 3) + 1; //Picks a random number between 1-3 and returns it
     if (choice === 1) {
@@ -20,32 +18,63 @@ function playRound(playerSelection, computerSelection) {
 
     if (player === "Rock") {
         if (computerSelection === "Scissors") {
-            return ("You Win! Rock beats Scissors");
+            console.log("You Win! Rock beats Scissors");
+            return 1;
         } else if (computerSelection === "Paper") {
-            return ("You Lose! Paper beats Rock");
+            console.log("You Lose! Paper beats Rock");
+            return 2;
         } else {
-            return ("Tie!");
+            console.log("Tie!");
+            return 0;
         }
     }
     if (player === "Paper") {
         if (computerSelection === "Rock") {
-            return ("You Win! Paper beats Rock");
+            console.log("You Win! Paper beats Rock");
+            return 1;
         } else if (computerSelection === "Scissors") {
-            return ("You Lose! Scissors beats Paper");
+            console.log("You Lose! Scissors beats Paper");
+            return 2;
         } else {
-            return ("Tie!");
+            console.log("Tie!");
+            return 0;
         }
     }
     if (player === "Scissors") {
         if (computerSelection === "Paper") {
-            return ("You Win! Scissors beats Paper");
+            console.log("You Win! Scissors beats Paper");
+            return 1;
         } else if (computerSelection === "Rock") {
-            return ("You Lose! Rock beats Scissors");
+            console.log("You Lose! Rock beats Scissors");
+            return 2;
         } else {
-            return ("Tie");
+            console.log("Tie");
+            return 0;
         }
     }
     else {
-        return ("Error! Player Selection Must Be (Rock, Paper, or Scissors)")
+        console.log("Error! Player Selection Must Be (Rock, Paper, or Scissors)")
+    }
+}
+
+function game() {
+    let winner = 0;
+    let loser = 0;
+
+    for (let i = 0; i < 5; i++ ) {
+        let currentRound = (playRound(prompt("Choose a weapon!"), computerPlay()));
+        if (currentRound === 1) {
+            winner++;
+        } else if (currentRound === 2) {
+            loser++;
+        }
+    }
+
+    if (winner > loser) {
+        console.log("You won the game! Refresh to play again.");
+    } else if (winner < loser) {
+        console.log("You lost! Refresh to play again.");
+    } else {
+        console.log("Tie Game! Refresh to play again.");
     }
 }
